2b149dd1b07b3ff1f43ba9bd42a6e92a3cc0f620
tetgrem <tetgrem@gmail.com>
2022-05-19 00:35:51 +0300

    header done

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,3 +1,4 @@
+// Custom Scripts
 // Мобильное меню бургер
 function burgerMenu() {
     const burger = document.querySelector('.burger')
@@ -39,3 +40,4 @@ function fixedNav() {
     }
 }
 window.addEventListener('scroll', fixedNav)
+
